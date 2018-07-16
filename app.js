55ecfa79ba4c4382c5f2ff97c70591817d8228ee
Andrés Martin <monkeyandres@protonmail.com>
2018-07-16 11:27:02 +0200

    Cambio desde rama Andres

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ hbs.registerHelper('ifUndefined', (value, options) => {
   
 
 // default value for title local
-app.locals.title = 'Express - Generated with IronGenerator';
+app.locals.title = 'MonkeyAndres';
 
 
 // Enable authentication using session + passport
