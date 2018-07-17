c4d73f3f915b1b6c58ffa6e60c8a7bb93bd513b8
Andrés Martin <monkeyandres@protonmail.com>
2018-07-17 11:41:56 +0200

    Add confirm mail with template

---
+++ mailing/index.js
@@ -0,0 +1,36 @@
+require('dotenv').config({path: '.env.priv'})
+
+const nodemailer = require('nodemailer');
+const hbs = require('nodemailer-express-handlebars');
+
+const options = {
+    viewEngine: {
+        extname: '.hbs',
+        layoutsDir: 'views/mailing',
+        partialsDir : 'views/partials/'
+    },
+    viewPath: 'views/mailing',
+    extName: '.hbs'
+};
+
+const transporter = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+        user: process.env.GMAILUSER,
+        pass: process.env.GMAILPASS
+    }
+});
+
+transporter.use('compile', hbs(options));
+
+const sendMail = (user) => {
+    let mail = {
+        to: user.email,
+        subject: 'Confirmate CrazyTrips Account!',
+        template: 'index',
+        context: {user}
+    };
+    return transporter.sendMail(mail)
+}
+
+module.exports = sendMail;
--- routes/auth.js
+++ routes/auth.js
@@ -1,103 +1,74 @@
 const express = require("express");
-const passport = require('passport');
+const passport = require("passport");
 const authRoutes = express.Router();
+
 const User = require("../models/User");
 const Trip = require("../models/Trip");
 const Request = require("../models/Request");
-const nodemailer = require("nodemailer");
 
+const nodemailer = require("nodemailer");
+const sendMail = require("../mailing");
 
 // Bcrypt to encrypt passwords
 const bcrypt = require("bcrypt");
 const bcryptSalt = 10;
 
-
 authRoutes.get("/login", (req, res, next) => {
-  res.render("auth/login", { "message": req.flash("error") });
+  res.render("auth/login", { message: req.flash("error") });
 });
 
 authRoutes.post("/login", passport.authenticate("local", {
-  successRedirect: "/trips",
-  failureRedirect: "/auth/login",
-  failureFlash: true,
-  passReqToCallback: true
-}));
+    successRedirect: "/trips",
+    failureRedirect: "/auth/login",
+    failureFlash: true,
+    passReqToCallback: true
+  })
+);
 
 authRoutes.get("/signup", (req, res, next) => {
   res.render("auth/signup");
 });
 
 authRoutes.post("/signup", (req, res, next) => {
-  
-  const {username,password,email,name,surname,sex,age,telephone,bio}=req.body
-  
+  const {username,password,email,name,surname,sex,age,telephone,bio} = req.body;
 
-  if (username === "" || password === "" || email==="" || name==="" || surname==="" || age==="" ||
-  telephone==="" || bio ==="" ) {
+  if (username === "" || password === "" || email === "" || name === "" || 
+      surname === "" || age === "" || telephone === "" || bio === "") {
     res.render("auth/signup", { message: "Fill all the information!!" });
     return;
   }
 
+  const salt = bcrypt.genSaltSync(bcryptSalt);
+  const hashPass = bcrypt.hashSync(password, salt);
 
-    const salt = bcrypt.genSaltSync(bcryptSalt);
-    const hashPass = bcrypt.hashSync(password, salt);
+  const newUser = new User({
+    username,
+    password: hashPass,
+    email,
+    name,
+    surname,
+    sex,
+    age,
+    telephone,
+    bio
+  });
 
-    const newUser = new User({
-      username, password: hashPass, email, name, surname, sex, age, telephone, bio
-    
+  newUser.save()
+    .then(data => {
+      return sendMail(newUser)
+    })
+    .then(() => {
+      res.redirect("/")
+    })
+    .catch(err => {
+      console.log(err);
+      res.render("auth/signup", { message: "User/email already exists!!" })
     });
-   console.log(newUser)
-    newUser.save((err) => {
-      if (err) {
-        res.render("auth/signup", { message: "User/email already exists!!" });
-      } else {
-
-        let email = req.body.email;
-        let transporter = nodemailer.createTransport({
-          service: 'Gmail',
-          auth: {
-            user: 'crazytrips@gmail.com',
-            pass: '12345'
-          }
-        });
- //       transporter.sendMail({
-       
- //       })
-
-        res.redirect("/");
-      }
-    });
-
 });
 
 authRoutes.get("/logout", (req, res) => {
   req.logout();
   res.redirect("/");
 });
-
-router.post('/send-email', (req, res, next) => {
-  let { email, subject, message } = req.body;
-  let transporter = nodemailer.createTransport({
-    service: 'Gmail',
-    auth: {
-      user: 'your email address',
-      pass: 'your email password'
-    }
-  });
-  transporter.sendMail({
-    from: "Perro",
-    to: email, 
-    subject: subject, 
-    text: message,
-    html: `<b>${message}</b>`
-  })
-  .then(info => res.render('message', {email, subject, message, info}))
-  .catch(error => console.log(error));
-});
-
-
-
-
-
 
 module.exports = authRoutes;
