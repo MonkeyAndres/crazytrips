6383738122c6c87c9556e75b47e6f706c8b58e60
Andrés Martin <monkeyandres@protonmail.com>
2018-07-17 17:03:57 +0200

    Stylish Profile

--- routes/auth.js
+++ routes/auth.js
@@ -19,7 +19,7 @@ authRoutes.get("/login", (req, res, next) => {
 });
 
 authRoutes.post("/login", passport.authenticate("local", {
-    successRedirect: "/trips",
+    successRedirect: "/profile",
     failureRedirect: "/auth/login",
     failureFlash: true,
     passReqToCallback: true
