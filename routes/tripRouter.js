8938348440e0102c9cde9d2e5c8c48bc6751716c
Andrés Martin <monkeyandres@protonmail.com>
2018-07-17 19:49:21 +0200

    Request v2

--- routes/tripRouter.js
+++ routes/tripRouter.js
@@ -3,14 +3,21 @@ const router = express.Router();
 const { ifLogged } = require('../middleware/logged');
 
 const Trip = require("../models/Trip");
+const Request = require("../models/Request");
 const FroalaEditor = require("../node_modules/wysiwyg-editor-node-sdk/lib/froalaEditor.js");
-
+const ObjectId = require('mongoose').Types.ObjectId; 
 router.use(ifLogged);
 
 router.get("/", (req, res, next) => {
   Trip.find()
     .populate("creator")
+    .sort({created_at: -1})
+    .lean()
     .then(trips => {
+      for(trip of trips) {
+        trip.startDate = trip.startDate.toDateString();
+        trip.endDate = trip.endDate.toDateString();
+      }
       res.render("trips/index", { trips });
     });
 });
@@ -55,15 +62,22 @@ router.get('/:id',(req,res,next)=>{
 
   Trip.findById(req.params.id)
   .populate('creator')
+  .lean()
   .then(trip=>{
-  
-    res.render('trips/suscripted-trips',{trip})
+    trip.startDate = trip.startDate.toDateString();
+    trip.endDate = trip.endDate.toDateString();
+
+    Request.find({trip: req.params.id})
+    .then(request => {
+      let canRequest = true;
+      for(r of request){
+        if(r.user.toString() == req.user._id){
+          canRequest = false;
+        }
+      }
+      res.render('trips/show',{trip, canRequest})
+    })
   })
   .catch(error=>console.log(error))
-
 })
-
-
-
-
 module.exports = router;
