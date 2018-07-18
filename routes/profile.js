e7e5277b36c20f18b6c3a3c809844d7f18e3f659
Andrés Martin <monkeyandres@protonmail.com>
2018-07-18 10:19:05 +0200

    RP System Finished

--- routes/profile.js
+++ routes/profile.js
@@ -23,6 +23,19 @@ profileRoutes.get('/', ifLogged, (req,res,next)=>{
     .catch(err => next(err))
 })
 
+profileRoutes.get('/rp/:id', ifLogged, (req, res, next) => {
+    User.findById(req.params.id)
+    .then(user => {
+        if (user.rp.indexOf(req.user._id) < 0 && req.user._id != user._id.toString()) {
+            user.rp.push(req.user._id);
+        }
+        
+        user.save()
+        .then(data => res.redirect(`/profile/${user.username}`))
+    })
+    .catch(err => next(err));
+})
+
 profileRoutes.get('/:username', ifLogged, (req,res,next)=>{
     User.findOne({username: req.params.username})
     .then(user => {
@@ -33,7 +46,9 @@ profileRoutes.get('/:username', ifLogged, (req,res,next)=>{
                 trip.startDate = trip.startDate.toDateString();
                 trip.endDate = trip.endDate.toDateString();
             }
-            res.render('profile/index', {user, createdTrips, yourProfile: false});
+
+            if(user._id.toString() == req.user._id) var yourProfile = true;
+            res.render('profile/index', {user, createdTrips, yourProfile});
         })
     })
     .catch(err => next(err));
